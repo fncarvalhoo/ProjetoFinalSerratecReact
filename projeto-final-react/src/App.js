ebc1a0e147192c86279e52415f866ce0d6913374
pedromathias <pedrohmathias10@gmail.com>
2022-11-04 19:38:59 -0300

    consumo de pedido e categoria

--- projeto-final-react/src/App.js
+++ projeto-final-react/src/App.js
@@ -1,4 +1,3 @@
-import { Header } from "../src/components/header/index.js";
 import { BrowserRouter } from "react-router-dom";
 import { GlobalStyle } from "../src/styles/global.js";
 import { Home } from "./pages/Home/index.js";
@@ -7,7 +6,6 @@ export function App() {
   return (
     <BrowserRouter>
       <GlobalStyle />
-      <Header />
       <Home />
     </BrowserRouter>
   );
