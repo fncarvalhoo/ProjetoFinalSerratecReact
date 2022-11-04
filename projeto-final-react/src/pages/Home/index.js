833604b67d34656115e0ad806c3ba8a7c3fb55ca
Fernando Carvalho <fernandocarvalho2304@gmail.com>
2022-11-04 19:39:26 -0300

    Consumo Produto
    Consumo api produto

--- projeto-final-react/src/pages/Home/index.js
+++ projeto-final-react/src/pages/Home/index.js
@@ -1,22 +1,39 @@
 import { useState, useEffect } from "react";
 import clienteService from "../../services/requests/clienteService";
-
+import produtoService from "../../services/requests/produtoService";
 
 export function Home() {
-    const [clientes, setClients] = useState([]);
+  const [clientes, setClients] = useState([]);
 
-    useEffect(() => {
-        clienteService.getClients().then(response => {
-            console.log(response.data)
-            setClients(response.data);
-        }).catch(error => {
-            console.log(error)
-        })
-    },[])
+  useEffect(() => {
+    clienteService
+      .getClients()
+      .then((response) => {
+        console.log(response.data);
+        setClients(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-    return(
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
+  const [produtos, setProtudos] = useState([]);
+
+  useEffect(() => {
+    produtoService
+      .getProdutos()
+      .then((response) => {
+        console.log(response.data);
+        setProtudos(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>response.data</h1>
+    </div>
+  );
 }
